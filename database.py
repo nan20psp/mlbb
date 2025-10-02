dd782df66d8e3cc21d6eaf81836be9ddea4745b8
nan20psp <pyaesone10psp@gmail.com>
2025-10-02 10:20:54 +0630

    Create database.py

---
+++ database.py
@@ -0,0 +1,29 @@
+{
+  "users": {},
+  "stock": {
+    "MLBBbal": {},
+    "MLBBph": {},
+    "PUPG": {}
+  },
+  "receipts": {},
+  "price": 1000,
+  "payment": {
+    "Wave": {
+      "phone": "",
+      "name": ""
+    },
+    "KPay": {
+      "phone": "",
+      "name": ""
+    }
+  },
+  "topup_requests": {},
+  "prices": {
+    "MLBBbal": {},
+    "MLBBph": {},
+    "PUPG": {}
+  },
+  "sales_total": 0,
+  "pending_registrations": {},
+  "cleanup_done": false
+}
